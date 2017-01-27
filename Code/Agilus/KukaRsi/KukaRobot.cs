03427fc1a9cf5f7a4b749c8be6abae373531fd36
chrpal <christoph.pallasch@rwth-aachen.de>
2017-01-27 11:59:49 +0100

    Funktionierender Code

---
+++ Code/Agilus/KukaRsi/KukaRobot.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+// Import the base messages
+using KukaRsi.Messages;
+
+namespace KukaRsi
+{
+    /// <summary>
+    /// Provides a communication interface for KUKA robots via the RSI protocol
+    /// </summary>
+    /// <typeparam name="MessageToRobot">The template for messages to the robot</typeparam>
+    /// <typeparam name="MessageFromRobot">The template for messages from the robot</typeparam>
+    public sealed class KukaRobot<MessageToRobot, MessageFromRobot>
+        where MessageToRobot : BaseMessageToRobot
+        where MessageFromRobot : BaseMessageFromRobot
+    {
+        /// <summary>
+        /// Notifies about received messages
+        /// </summary>
+        public event Action<MessageFromRobot> Received;
+        /// <summary>
+        /// Notifies about changes of the connection state (connected, not connected)
+        /// </summary>
+        public event Action<bool> ConnectionStateChanged;
+
+        /// <summary>
+        /// Indicates whether the KUKA robot is connected
+        /// </summary>
+        public bool IsConnected { get; private set; }
+
+        // Local endpoint to listen on
+        private IPEndPoint LocalEndpoint;
+        // UDP communication client
+        private UdpClient Communicator;
+        // Communication thread
+        private Thread CommunicationThread;
+        // Communication stop flag
+        private volatile bool ShouldStop;
+
+        // Message serializer
+        private MessageSerialization<MessageToRobot, MessageFromRobot> Serialization;
+        // Cross-thread message pipe
+        private MessagePipe<MessageToRobot> Pipe;
+
+        // Default (empty) message to the robot
+        private MessageToRobot DefaultMessage;
+
+        /// <summary>
+        /// Creates a new KUKA robot communication
+        /// </summary>
+        /// <remarks>
+        /// This interface operates as a server and waits for incoming UDP requests from the robot.
+        /// Therefor, the local endpoint parameter must be a local endpoint, not the robot endpoint!
+        /// </remarks>
+        /// <param name="localEndpoint">The local network interface to open the UDP server</param>
+        /// <param name="defaultMessage">The default message for non-stop idle communication</param>
+        public KukaRobot(IPEndPoint localEndpoint, MessageToRobot defaultMessage)
+        {
+            this.IsConnected = false;
+            this.LocalEndpoint = localEndpoint;
+            this.Serialization = new MessageSerialization<MessageToRobot, MessageFromRobot>();
+            this.Pipe = new MessagePipe<MessageToRobot>();
+            this.DefaultMessage = defaultMessage;
+        }
+
+        /// <summary>
+        /// Starts listening for UDP (RSI) requests
+        /// </summary>
+        public void Start()
+        {
+            this.ShouldStop = false;
+            // Use a background thread for communication
+            this.CommunicationThread = new Thread(() => this.Run()) { IsBackground = true };
+            this.CommunicationThread.Start();
+        }
+
+        /// <summary>
+        /// Stops listening for UDP requests
+        /// </summary>
+        public void Stop()
+        {
+            this.ShouldStop = true;
+            // Wait for the thread to stop correctly
+            this.CommunicationThread.Join();
+        }
+
+        /// <summary>
+        /// Communication loop
+        /// </summary>
+        private void Run()
+        {
+            this.Communicator = new UdpClient();
+            this.Communicator.Client.Bind(this.LocalEndpoint);
+            // Reconnect until should stop
+            while (!this.ShouldStop)
+            {
+                try
+                {
+                    // Wait for the first message from the robot
+                    this.Communicator.Client.ReceiveTimeout = 50;
+                    IPEndPoint targetEndpoint = null;
+                    // Receive
+                    byte[] receivedBytes = this.Communicator.Receive(ref targetEndpoint);
+                    // Decode
+                    string receivedString = Encoding.ASCII.GetString(receivedBytes);
+                    // Deserialize
+                    MessageFromRobot receivedMessage = this.Serialization.Deserialize(receivedString);
+                    this.RaiseReceived(receivedMessage);
+                    // First received message indicates an established connection
+                    this.SetConnectionState(true);
+                    // Predefine for sending
+                    MessageToRobot commandMessage, sendMessage;
+                    string sendString;
+                    byte[] sendBytes;
+                    // Connect to the robot
+                    this.Communicator.Connect(targetEndpoint);
+                    this.Communicator.Client.ReceiveTimeout = 15;
+                    // Message loop until should stop
+                    while (!this.ShouldStop)
+                    {
+                        // Check for a stored message, otherwise use default
+                        commandMessage = this.Pipe.Pull();
+                        sendMessage = (commandMessage != null) ? commandMessage : this.DefaultMessage;
+                        // Copy IPOC from the last received message
+                        sendMessage.SetIpoc(receivedMessage.Ipoc);
+                        // Serialize
+                        sendString = this.Serialization.Serialize(sendMessage);
+                        // Encode
+                        sendBytes = Encoding.ASCII.GetBytes(sendString);
+                        // Send
+                        this.Communicator.Send(sendBytes, sendBytes.Length);
+                        // Receive
+                        receivedBytes = this.Communicator.Receive(ref targetEndpoint);
+                        // Decode
+                        receivedString = Encoding.ASCII.GetString(receivedBytes);
+                        // Deserialize
+                        receivedMessage = this.Serialization.Deserialize(receivedString);
+                        this.RaiseReceived(receivedMessage);
+                    }
+                } 
+                catch (SocketException)
+                {
+                    // Catch communication errors as well as timeouts => disconnected
+                    this.SetConnectionState(false);
+                }
+            }
+            this.Communicator.Close();
+        }
+
+        /// <summary>
+        /// Sends a message to the robot
+        /// </summary>
+        /// <param name="message">The message to send</param>
+        public void Send(MessageToRobot message)
+        {
+            this.Pipe.Push(message);
+        }
+
+        private void RaiseReceived(MessageFromRobot message)
+        {
+            // Raise in another thread instead of communication thread
+            this.Received.BeginInvoke(message, this.Received.EndInvoke, null);
+        }
+
+        private void SetConnectionState(bool connectionState)
+        {
+            // Only modify if the state changed
+            if (this.IsConnected != connectionState)
+            {
+                this.IsConnected = connectionState;
+                if (this.ConnectionStateChanged != null)
+                {
+                    // Raise in another thread instead of communication thread
+                    this.ConnectionStateChanged.BeginInvoke(connectionState, this.ConnectionStateChanged.EndInvoke, null);
+                }
+            }
+        }
+    }
+}
---
+++ Code/Agilus/KukaRsi/MessagePipe.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+// Import base message types
+using KukaRsi.Messages;
+
+namespace KukaRsi
+{
+    /// <summary>
+    /// Stores a message object for access from different threads
+    /// </summary>
+    /// <typeparam name="MessageToRobot">The used message template</typeparam>
+    internal class MessagePipe<MessageToRobot>
+        where MessageToRobot : BaseMessageToRobot
+    {
+        /// <summary>
+        /// A lock to avoid synchronous access on the stored message
+        /// </summary>
+        private Object InternalLock;
+
+        /// <summary>
+        /// The stored message object
+        /// </summary>
+        private MessageToRobot CurrentMessage;
+
+        /// <summary>
+        /// Initializes a new MessagePipe
+        /// </summary>
+        public MessagePipe()
+        {
+            this.InternalLock = new Object();
+        }
+
+        /// <summary>
+        /// Stores a message object for cross-thread accesss
+        /// </summary>
+        /// <remarks>This can be used on any thread</remarks>
+        /// <param name="message">The stored message</param>
+        /// <returns>Whether the message could be stored</returns>
+        public bool Push(MessageToRobot message)
+        {
+            bool alreadyAssigned = false;
+            lock (this.InternalLock)
+            {
+                // Check for previous stored message
+                if (this.CurrentMessage == null)
+                {
+                    this.CurrentMessage = message;
+                }
+                else
+                {
+                    alreadyAssigned = true;
+                }
+            }
+            return !alreadyAssigned;
+        }
+
+        /// <summary>
+        /// Returns and resets the stored message object
+        /// </summary>
+        /// <remarks>This should be used on the communication thread</remarks>
+        /// <returns></returns>
+        public MessageToRobot Pull()
+        {
+            MessageToRobot message;
+            lock (this.InternalLock)
+            {
+                message = this.CurrentMessage;
+                this.CurrentMessage = null;
+            }
+            return message;
+        }
+
+    }
+}
